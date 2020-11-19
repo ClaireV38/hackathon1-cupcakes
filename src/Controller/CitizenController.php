14a0ceb0267e168819ac2898d5d963a7f9eca795
Florian-Rowehy <florian.rowehy@gmail.com>
2020-11-19 19:44:56 +0100

    moved picture management to citizen controller & citizen/index.html.twig

--- src/Controller/CitizenController.php
+++ src/Controller/CitizenController.php
@@ -4,5 +4,18 @@ namespace App\Controller;
 
 class CitizenController extends AbstractController
 {
-
+    public function index()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $img = $_POST['photo'] ?? "";
+            $img = str_replace('data:image/png;base64,', '', $img);
+            $img = str_replace(' ', '+', $img);
+            $data = base64_decode($img);
+            $fileName = md5(uniqid("" . rand() . time(), true)) . '.png';
+            $file = ROOTPATH . '/public/upload/' . $fileName;
+            $success = file_put_contents($file, $data);
+            var_dump($success);
+        }
+        return $this->twig->render('Citizen/index.html.twig');
+    }
 }
