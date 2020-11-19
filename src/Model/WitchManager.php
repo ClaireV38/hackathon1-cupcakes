d2a148a095af6a98d1a044b73f0bd76d244a2140
JeanneSic <siche.jeanne@gmail.com>
2020-11-19 15:20:07 +0100

    display all witches in inquisitor view bounty

--- src/Model/WitchManager.php
+++ src/Model/WitchManager.php
@@ -6,5 +6,10 @@ namespace App\Model;
 
 class WitchManager extends AbstractManager
 {
+    const TABLE = 'witch';
 
+    public function __construct()
+    {
+         parent::__construct(self::TABLE);
+    }
 }
