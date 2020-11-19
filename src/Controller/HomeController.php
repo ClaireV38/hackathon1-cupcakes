5e868fad0b772770d0cc24d2dd000bbc8a471d16
Florian-Rowehy <florian.rowehy@gmail.com>
2020-11-19 19:23:06 +0100

    can save snapshot through form and save it public/upload

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,6 +21,16 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $img = $_POST['photo'] ?? "";
+            $img = str_replace('data:image/png;base64,', '', $img);
+            $img = str_replace(' ', '+', $img);
+            $data = base64_decode($img);
+            $fileName = md5(uniqid( "".rand().time(), true)).'.png';
+            $file = ROOTPATH . '/public/upload/' . $fileName;
+            $success = file_put_contents($file, $data);
+            var_dump($success);
+        }
         return $this->twig->render('Home/index.html.twig');
     }
 }
