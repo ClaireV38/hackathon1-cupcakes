d345d622ea508121ecbc288507c085597d8d3d96
Raphael Liere <raphael.liere@hotmail.fr>
2020-11-19 16:17:57 +0100

    Adding a register form

--- src/Controller/InquisitorController.php
+++ src/Controller/InquisitorController.php
@@ -1,10 +1,51 @@
 <?php
 
-
 namespace App\Controller;
 
+use App\Model\InquisitorManager;
 
 class InquisitorController extends AbstractController
 {
+    public function signUp()
+    {
+        if (!empty($_SESSION)) {
+            header('Location: /');
+        }
 
+        $inquisitor = [];
+        $errors = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === "POST" && !empty($_POST)) {
+            $inquisitor['name'] = trim($_POST['name']);
+            $inquisitor['registrationNumber'] = trim($_POST['registrationNumber']);
+            $inquisitor['password'] = trim($_POST['password']);
+
+            if (empty($inquisitor['name'])) {
+                $errors['name'] = 'Required';
+            }
+            if (empty($inquisitor['registrationNumber'])) {
+                $errors['registrationNumber'] = 'Required';
+            } elseif (strlen($inquisitor['registrationNumber']) !== 8) {
+                $errors['registrationNumber'] = 'Registration number must contains 8 figures';
+            }
+            if (empty($inquisitor['password'])) {
+                $errors['password'] = 'Required';
+            }
+
+            if (empty($errors)) {
+                $inquisitorManager = new InquisitorManager();
+                try {
+                    $inquisitorManager->addInquisitor($inquisitor);
+                    header('Location:/inquisitor/signin');
+                } catch (\PDOException $e) {
+                    $errors['form'] = 'Registration number already use by an inquisitor, please contact the Kingdom';
+                }
+            }
+        }
+
+        return $this->twig->render('Inquisitor/signUp.html.twig', [
+            'errors' => $errors,
+            'inquisitor' => $inquisitor
+        ]);
+    }
 }
--- src/Model/InquisitorManager.php
+++ src/Model/InquisitorManager.php
@@ -4,5 +4,23 @@ namespace App\Model;
 
 class InquisitorManager extends AbstractManager
 {
+    const TABLE = 'inquisitor';
 
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+    public function addInquisitor($inquisitor)
+    {
+        $query = 'INSERT INTO ' . self::TABLE . ' (name, registrationNumber, password)
+        VALUES (:name, :registrationNumber, :password)';
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('name', $inquisitor['name'], \PDO::PARAM_STR);
+        $statement->bindValue('registrationNumber', $inquisitor['registrationNumber'], \PDO::PARAM_INT);
+        $statement->bindValue('password', password_hash($inquisitor['password'], PASSWORD_DEFAULT), \PDO::PARAM_STR);
+        if ($statement->execute()) {
+            return $this->pdo->lastInsertId();
+        }
+    }
 }
